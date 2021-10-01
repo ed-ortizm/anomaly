a157e35b0de3ecc7bc750618696f845209601f97
Edgar Ortiz <ed.ortizm@gmail.com>
2021-10-01 18:47:07 -0300

    Run black formatter. Merge Generative branch

--- src/reconstruction.py
+++ src/reconstruction.py
@@ -1,46 +1,46 @@
 import sys
 import numpy as np
+
 ################################################################################
 class ReconstructionAnomalyScore:
     """
     Class for dealing with the outliers based on a generative model trained with
     tensorflow.keras
     """
+
     ############################################################################
     def __init__(self,):
         pass
+
     ###########################################################################
-    def _update_dimensions(self, x: "np.array")->"numpy array":
+    def _update_dimensions(self, x: "np.array") -> "numpy array":
 
         if x.ndim == 1:
             x = x[np.newaxis, ...]
 
         return x
+
     ###########################################################################
-    def _get_largest_pixels_error_ids(self,
-        flux_wise_error: "numpy array",
-        percentage: "int",
-        )->"numpy array":
+    def _get_largest_pixels_error_ids(
+        self, flux_wise_error: "numpy array", percentage: "int"
+    ) -> "numpy array":
 
         number_fluxes = flux_wise_error.shape[1]
-        number_anomalous_fluxes = int(0.01*percentage*number_fluxes)
+        number_anomalous_fluxes = int(0.01 * percentage * number_fluxes)
 
         largest_pixels_error_ids = np.argpartition(
-            flux_wise_error,
-            -1 * number_anomalous_fluxes,
-            axis=1
-        )[:, -1 * number_anomalous_fluxes:]
+            flux_wise_error, -1 * number_anomalous_fluxes, axis=1
+        )[:, -1 * number_anomalous_fluxes :]
 
         return larger_pixels_error_ids
+
     ###########################################################################
-    def _get_anomaly_score(self,
-        flux_wise_error: "numpy_array",
-        percentage: "int",
-        )->"numpy array":
+    def _get_anomaly_score(
+        self, flux_wise_error: "numpy_array", percentage: "int"
+    ) -> "numpy array":
 
         largest_pixels_error_ids = self._get_largest_pixels_error_ids(
-            flux_wise_error,
-            percentage,
+            flux_wise_error, percentage
         )
 
         anomaly_score = np.empty(largest_pixels_error_ids.shape)
@@ -50,12 +50,14 @@ class ReconstructionAnomalyScore:
             anomaly_score[n, :] = flux_wise_error[n, idx]
 
         return np.mean(anomaly_score, axis=1)
+
     ###########################################################################
-    def mse(self,
+    def mse(
+        self,
         observation: "numpy array",
         reconstruction: "numpy array",
         percentage: "int",
-        )-> "numpy array":
+    ) -> "numpy array":
 
         """
         PARAMETERS
@@ -74,23 +76,28 @@ class ReconstructionAnomalyScore:
         anomaly_score = self._get_anomaly_score(flux_wise_error, percentage)
 
         return anomaly_score
+
     ############################################################################
-    def mse_relative(self,
+    def mse_relative(
+        self,
         observation: "numpy array",
         reconstruction: "numpy array",
         percentage: "int",
         epsilon: "float" = 1e-3,
-        )-> "numpy array":
+    ) -> "numpy array":
 
         flux_wise_error = np.square(reconstruction - observation)
-        flux_wise_error *= 1./np.square(O + epsilon)
+        flux_wise_error *= 1.0 / np.square(O + epsilon)
 
         flux_wise_error = self._update_dimensions(flux_wise_error)
         anomaly_score = self._get_anomaly_score(flux_wise_error, percentage)
 
         return anomaly_score
+
     ############################################################################
     ############################################################################
+
+
 #     def _mad(self, O, R, percentage, image):
 #
 #         reconstruction = np.abs(R - O)
