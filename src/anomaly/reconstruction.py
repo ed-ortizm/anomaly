6415f53da052b5c994c1a2694b5fc6eddbdd410f
Edgar Ortiz <ed.ortizm@gmail.com>
2021-11-02 15:47:31 -0300

    Document functionality. Weight relative anomaly scores with the reconstruction of the observed spectra

--- src/anomaly/reconstruction.py
+++ src/anomaly/reconstruction.py
@@ -1,14 +1,14 @@
 import sys
 import numpy as np
 
-################################################################################
+###############################################################################
 class ReconstructionAnomalyScore:
     """
     Class for dealing with the outliers based on a generative model trained with
     tensorflow.keras
     """
 
-    ############################################################################
+    ###########################################################################
     def __init__(self,
         model: "tf.keras.model"
         ):
@@ -40,6 +40,11 @@ class ReconstructionAnomalyScore:
         )->"numpy array":
 
         """
+        Computes the ids of the pixels with the largest reconstruction
+            errors. If percentage is 100, then it does nothing.
+            If percentage is 30%, for instance, it returns the ids of
+            30% of the pixels with the highest reconstruction errors.
+
         PARAMETERS
             flux_wise_error: array with reconstruction errors
                 flux by flux
@@ -47,7 +52,8 @@ class ReconstructionAnomalyScore:
                 reconstruction error to consider to compute
                 the anomaly score
         OUTPUT
-            anomaly score of the input observation
+            largest_reconstruction_error_ids: ids with the percentage
+                of pixels with the highest reconstruction errors
         """
 
         number_fluxes = flux_wise_error.shape[1]
@@ -56,12 +62,15 @@ class ReconstructionAnomalyScore:
         largest_reconstruction_error_ids = np.argpartition(
             flux_wise_error,
             -1 * number_anomalous_fluxes,
-            axis=1)[:, -1 * number_anomalous_fluxes:]
+            axis=1
+        )[:, -1 * number_anomalous_fluxes:]
 
         return largest_reconstruction_error_ids
     ###########################################################################
     def _get_anomaly_score(
-        self, flux_wise_error: "numpy_array", percentage: "int"
+        self,
+        flux_wise_error: "numpy_array",
+        percentage: "int"
     ) -> "numpy array":
 
         """
@@ -72,7 +81,7 @@ class ReconstructionAnomalyScore:
                 reconstruction error to consider to compute
                 the anomaly score
         OUTPUT
-            anomaly score of the input observation
+            mean anomaly score of the input observation
         """
 
         reconstruction_error_ids = self._get_reconstruction_error_ids(
@@ -82,9 +91,9 @@ class ReconstructionAnomalyScore:
 
         anomaly_score = np.empty(reconstruction_error_ids.shape)
 
-        for n, idx in enumerate(reconstruction_error_ids):
+        for idx, reconstruction_id in enumerate(reconstruction_error_ids):
 
-            anomaly_score[n, :] = flux_wise_error[n, idx]
+            anomaly_score[idx, :] = flux_wise_error[idx, reconstruction_id]
 
         return np.mean(anomaly_score, axis=1)
 
@@ -113,7 +122,7 @@ class ReconstructionAnomalyScore:
         flux_wise_error = np.square(reconstruction - observation)
 
         if relative:
-            flux_wise_error *= 1./np.square(observation + epsilon)
+            flux_wise_error *= 1./np.square(reconstruction + epsilon)
 
         flux_wise_error = self._update_dimensions(flux_wise_error)
 
@@ -145,7 +154,7 @@ class ReconstructionAnomalyScore:
         flux_wise_error = np.abs(reconstruction - observation)
 
         if relative:
-            flux_wise_error *= 1./np.square(observation + epsilon)
+            flux_wise_error *= 1./np.abs(reconstruction + epsilon)
 
         flux_wise_error = self._update_dimensions(flux_wise_error)
 
@@ -153,71 +162,6 @@ class ReconstructionAnomalyScore:
 
         return anomaly_score
     ###########################################################################
-#     def _mad(self, O, R, percentage, image):
-#
-#         reconstruction = np.abs(R - O)
-#
-#         number_outlier_fluxes = int(0.01*percentage*reconstruction.shape[1])
-#
-#         largest_reconstruction_ids = np.argpartition(
-#             reconstruction,
-#             -1 * number_outlier_fluxes,
-#             axis=1)[:, -1 * number_outlier_fluxes:]
-#
-#         score = np.empty(largest_reconstruction_ids.shape)
-#
-#         for n, idx in enumerate(largest_reconstruction_ids):
-#
-#             score[n, :] = reconstruction[n, idx]
-#
-#         o_score = score.sum(axis=1)
-#
-#         if image:
-#
-#             similarity =  o_score.max() - o_score
-#
-#             o_similarity = np.empty((o_score.size, 2))
-#             o_similarity[:, 0] = o_score[:]
-#             o_similarity[:, 1] = similarity[:]
-#
-#             return o_similarity
-#
-#         return o_score
-#     ############################################################################
-#     def _mad_relative(self, O, R, percentage, image):
-#
-#         if image:
-#             O, R = O[:, 0, :, 0], R[:, 0, :, 0]
-#
-#         reconstruction = np.abs( (R - O)/(O + self.epsilon) )
-#
-#         number_outlier_fluxes = int(0.01*percentage*reconstruction.shape[1])
-#
-#         largest_reconstruction_ids = np.argpartition(
-#             reconstruction,
-#             -1 * number_outlier_fluxes,
-#             axis=1)[:, -1 * number_outlier_fluxes:]
-#
-#         score = np.empty(largest_reconstruction_ids.shape)
-#
-#         for n, idx in enumerate(largest_reconstruction_ids):
-#
-#             score[n, :] = reconstruction[n, idx]
-#
-#         o_score = np.sum(score, axis=1)
-#         if image:
-#
-#             similarity =  o_score.max() - o_score
-#
-#             o_similarity = np.empty((o_score.size, 2))
-#             o_similarity[:, 0] = o_score[:]
-#             o_similarity[:, 1] = similarity[:]
-#
-#             return o_similarity
-#
-#         return o_score
-#     ############################################################################
-#     ############################################################################
 #     def _lp(self, O, R, percentage, image):
 #
 #         if image:
@@ -285,8 +229,7 @@ class ReconstructionAnomalyScore:
 #             return o_similarity
 #
 #         return o_score
-#     ############################################################################
-#     ############################################################################
+    ###########################################################################
 #     def top_reconstructions(self, scores, n_top_spectra):
 #         """
 #         Selects the most normal and outlying objecs
@@ -312,34 +255,3 @@ class ReconstructionAnomalyScore:
 #         most_oulying_ids = spec_idxs[-1 * n_top_spectra:]
 #
 #         return most_normal_ids, most_oulying_ids
-# ################################################################################
-#     # def _mse(self, O, R, percentage, image):
-#     #
-#     #     if image:
-#     #         O, R = O[:, 0, :, 0], R[:, 0, :, 0]
-#     #
-#     #     mse = np.abs(R - O)/np.abs(O+0.0001)
-#     #
-#     #     number_outlier_fluxes = int(percentage*mse.shape[1])
-#     #     highest_mse = np.argpartition(mse, -1 * number_outlier_fluxes,
-#     #         axis=1)[:, -1 * number_outlier_fluxes:]
-#     #
-#     #     score = np.empty(highest_mse.shape)
-#     #
-#     #     for n, idx in enumerate(highest_mse):
-#     #
-#     #         score[n, :] = mse[n, idx]
-#     #
-#     #     o_score = score.sum(axis=1)
-#     #
-#     #     if image:
-#     #
-#     #         similarity =  o_score.max() - o_score
-#     #
-#     #         o_similarity = np.empty((o_score.size, 2))
-#     #         o_similarity[:, 0] = o_score[:]
-#     #         o_similarity[:, 1] = similarity[:]
-#     #
-#     #         return o_similarity
-#     #
-#     #     return o_score
