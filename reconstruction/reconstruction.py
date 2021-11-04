23bb2ea2faf2cecded200d122486c7369de4743c
Edgar Ortiz <ed.ortizm@gmail.com>
2021-11-04 17:13:23 -0300

    Verify mse works with other repos

--- reconstruction/reconstruction.py
+++ reconstruction/reconstruction.py
@@ -10,7 +10,9 @@ from anomaly.analysis import AnalysisAnomalyScore
 from anomaly.reconstruction import ReconstructionAnomalyScore
 
 from autoencoders.deterministic.autoencoder import AE
-from sdss.superclasses import FileDirectory
+from autoencoders.variational.autoencoder import VAE
+
+# from sdss.superclasses import FileDirectory
 
 ###############################################################################
 if __name__ == "__main__":
@@ -21,7 +23,7 @@ if __name__ == "__main__":
     parser = ConfigParser(interpolation=ExtendedInterpolation())
     parser.read("reconstruction.ini")
     # Check files and directory
-    check = FileDirectory()
+    # check = FileDirectory()
     ###########################################################################
     # Load model
     model_type = parser.get("directories", "model_type")
@@ -31,6 +33,30 @@ if __name__ == "__main__":
     if model_type == "ae":
 
         model = AE.load(model_directory)
+
+    elif model_type == "vae":
+
+        model = VAE.load(model_directory)
+
+    ###########################################################################
+    # Load data
+    input_data_directory = parser.get("directories", "input")
+    train_set_name = parser.get("files", "observations")
+    observations = np.load(f"{input_data_directory}/{train_set_name}")
+    ###########################################################################
+    # Load class to compute scores
+    analyze_anomalies = ReconstructionAnomalyScore(model)
+    # metric parameters
+    metric = parser.get("score", "metric")
+    relative = parser.getboolean("score", "relative")
+    percentage = parser.getfloat("score", "percentage")
+
+    if metric == "mse":
+        anomaly_score = analyze_anomalies.mse(
+            observation=observations,
+            percentage=percentage,
+            relative=relative,
+        )
 
     ###########################################################################
     finish_time = time.time()
