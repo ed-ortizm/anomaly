f14a4148afbfde164b40e6f81b7ad9ffd489a3dd
Edgar Ortiz <ed.ortizm@gmail.com>
2021-11-13 14:36:23 -0300

    Delete irelevant entries in config file for reconstruction script, add code to get reconstruction array. Metric is saved in the desired format: mse_relative_True_percentage_20

--- reconstruction/reconstruction.py
+++ reconstruction/reconstruction.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python3
 from configparser import ConfigParser, ExtendedInterpolation
-import multiprocessing as mp
 import time
 
 import numpy as np
@@ -15,61 +14,71 @@ from autoencoders.variational.autoencoder import VAE
 from sdss.superclasses import FileDirectory
 
 ###############################################################################
-if __name__ == "__main__":
-    mp.set_start_method("spawn")
-
-    start_time = time.time()
-    ###########################################################################
-    parser = ConfigParser(interpolation=ExtendedInterpolation())
-    parser.read("reconstruction.ini")
-    # Check files and directory
-    check = FileDirectory()
-    ###########################################################################
-    # Load model
-    model_type = parser.get("common", "model_type")
-    model_directory = parser.get("directories", "model")
+start_time = time.time()
+###########################################################################
+parser = ConfigParser(interpolation=ExtendedInterpolation())
+parser.read("reconstruction.ini")
+# Check files and directory
+check = FileDirectory()
+###########################################################################
+# Load model
+model_type = parser.get("common", "model_type")
+model_directory = parser.get("directories", "model")
 
 
-    if model_type == "ae":
+if model_type == "ae":
 
-        model = AE.load(model_directory)
+    model = AE.load(model_directory)
 
-    elif model_type == "vae":
+elif model_type == "vae":
 
-        model = VAE.load(model_directory)
+    model = VAE.load(model_directory)
 
-    ###########################################################################
-    # Load data
-    input_data_directory = parser.get("directories", "input")
-    train_set_name = parser.get("files", "observations")
-    observations = np.load(f"{input_data_directory}/{train_set_name}")
-    ###########################################################################
-    # Load class to compute scores
-    analyze_anomalies = ReconstructionAnomalyScore(model)
-    # metric parameters
-    metric = parser.get("score", "metric")
-    relative = parser.getboolean("score", "relative")
-    percentage = parser.getfloat("score", "percentage")
+###########################################################################
+# Load data
+input_data_directory = parser.get("directories", "input")
+train_set_name = parser.get("files", "observations")
+observations = np.load(f"{input_data_directory}/{train_set_name}")
+###########################################################################
+# Load class to compute scores
+analyze_anomalies = ReconstructionAnomalyScore(model)
+# metric parameters
+metric = parser.get("score", "metric")
+relative = parser.getboolean("score", "relative")
+percentage = parser.getfloat("score", "percentage")
 
-    if metric == "mse":
-        anomaly_score = analyze_anomalies.mse(
-            observation=observations,
-            percentage=percentage,
-            relative=relative,
-        )
+if metric == "mse":
+    anomaly_score = analyze_anomalies.mse(
+        observation=observations,
+        percentage=percentage,
+        relative=relative,
+    )
 
-    save_scores = parser.getboolean("parameters", "save_scores")
+output_directory = parser.get("directories", "output")
+check.check_directory(output_directory, exit=False)
 
-    if save_scores:
+save_scores = parser.getboolean("parameters", "save_scores")
 
-        output_directory = parser.get("directories", "output")
-        check.check_directory(output_directory, exit=False)
+if save_scores:
 
-        anomaly_score_name = parser.get("files","anomaly_score")
-        save_to = f"{output_directory}/{anomaly_score_name}"
+    anomaly_score_name = parser.get("files", "anomaly_score")
+    save_to = f"{output_directory}/{anomaly_score_name}"
 
-        np.save(save_to, anomaly_score)
+    np.save(save_to, anomaly_score)
+    print(save_to)
 
-    ###########################################################################
-    finish_time = time.time()
-    print(f"Run time: {finish_time - start_time:.2f}")
+save_reconstruction = parser.getboolean("parameters", "save_reconstruction")
+
+if save_reconstruction:
+
+    reconstructions = model.reconstruct(observations)
+
+    reconstructions_name = parser.get("files", "reconstructions")
+    save_to = f"{model_directory}/{reconstructions_name}"
+
+    np.save(save_to, observations)
+    print(save_to)
+
+###########################################################################
+finish_time = time.time()
+print(f"Run time: {finish_time - start_time:.2f}")
