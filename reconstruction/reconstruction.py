aa191735453b9b317bc988d62ba17e9274a47edc
Edgar Ortiz <ed.ortizm@gmail.com>
2021-11-20 21:43:31 -0300

    Set reconstruction.ini to save many parameters

--- reconstruction/reconstruction.py
+++ reconstruction/reconstruction.py
@@ -14,16 +14,15 @@ from sdss.superclasses import FileDirectory
 
 ###############################################################################
 start_time = time.time()
-###########################################################################
+###############################################################################
 parser = ConfigParser(interpolation=ExtendedInterpolation())
 parser.read("reconstruction.ini")
 # Check files and directory
 check = FileDirectory()
-###########################################################################
+###############################################################################
 # Load model
 model_type = parser.get("common", "model_type")
 model_directory = parser.get("directories", "model")
-
 
 if model_type == "ae":
 
@@ -33,14 +32,18 @@ elif model_type == "vae":
 
     model = VAE.load(model_directory)
 
-###########################################################################
-# Load data
-input_data_directory = parser.get("directories", "input")
-train_set_name = parser.get("files", "observations")
-observations = np.load(f"{input_data_directory}/{train_set_name}")
 ###############################################################################
-reconstructions_name = parser.get("files", "reconstructions")
-reconstruction_location = f"{model_directory}/{reconstructions_name}"
+# Load data
+
+input_data_directory = parser.get("directories", "input")
+
+train_set_name = parser.get("files", "observation")
+observation = np.load(f"{input_data_directory}/{train_set_name}")
+
+#######################################################################
+
+reconstruction_name = parser.get("files", "reconstruction")
+reconstruction_location = f"{model_directory}/{reconstruction_name}"
 
 reconstruction_in_drive = parser.getboolean(
     "parameters", "reconstruction_in_drive"
@@ -48,45 +51,69 @@ reconstruction_in_drive = parser.getboolean(
 
 if not reconstruction_in_drive:
 
-    reconstruction = model.reconstruct(observations)
-
-    # reconstructions_name = parser.get("files", "reconstructions")
-    # save_to = f"{model_directory}/{reconstructions_name}"
-
+    reconstruction = model.reconstruct(observation)
     np.save(reconstruction_location, reconstruction)
 
 else:
+
     reconstruction = np.load(reconstruction_location)
 ###############################################################################
 # Load class to compute scores
-analyze_anomalies = ReconstructionAnomalyScore(model)
+get_score = ReconstructionAnomalyScore(model)
 # metric parameters
 metric = parser.get("score", "metric")
-relative = parser.getboolean("score", "relative")
+# relative = parser.getboolean("score", "relative")
+relative_values = [True, False]
+
 percentage = parser.getfloat("score", "percentage")
+percentage = percentage.replace(" ", "").split(",")
+percentage_values = [int(value) for value in percentage]
 
-if metric == "mse":
+# specobjid to save anomaly scores in data frame
+train_id_name = parser.get("files", "train_id")
+indexes_interpolate = np.load(f"{input_data_directory}/{train_id_name}")
 
-    anomaly_score = analyze_anomalies.mse(
-        observation=observations,
-        percentage=percentage,
-        relative=relative,
-        reconstruction_in_drive = reconstruction_in_drive,
-        reconstruction = reconstruction
+data_frame = pd.DataFrame(
+    columns=["specobjid"],
+    data=indexes_interpolate[:, 1]
     )
+
+for relative in relative_values:
+
+    for percentage in percentage_values:
+
+        if metric == "mse":
+
+            anomaly_score = get_score.mse(
+                observation=observation,
+                percentage=percentage,
+                relative=relative,
+                reconstruction_in_drive = reconstruction_in_drive,
+                reconstruction = reconstruction
+            )
+        #######################################################################
+        # Save anomaly scores
+        output_directory = parser.get("directories", "output")
+        check.check_directory(output_directory, exit=False)
+
+        save_scores = parser.getboolean("parameters", "save_scores")
+
+        if save_scores:
+
+            anomaly_score_name = (
+                f"${metric}_relative_${relative}_percentage_${percentage}"
+            )
+
+            save_to = f"{output_directory}/{anomaly_score_name}"
+
+            np.save(f"{save_to}.npy", anomaly_score)
+        #######################################################################
+        # save to data frame
+        data_frame['anomalyScore'] = anomaly_score
+
+        data_frame.to_csv(f"{save_to}.csv.gz", index=False)
 ###############################################################################
-output_directory = parser.get("directories", "output")
-check.check_directory(output_directory, exit=False)
 
-save_scores = parser.getboolean("parameters", "save_scores")
 
-if save_scores:
-
-    anomaly_score_name = parser.get("files", "anomaly_score")
-    save_to = f"{output_directory}/{anomaly_score_name}"
-
-    np.save(save_to, anomaly_score)
-    print(save_to)
-###########################################################################
 finish_time = time.time()
 print(f"Run time: {finish_time - start_time:.2f}")
--- reconstruction/top_scores.py
+++ reconstruction/top_scores.py
@@ -24,13 +24,6 @@ anomaly_score = np.load(f"{input_output_directory}/{anomaly_score_name}")
 # Load class to analyse anomaies
 analyze = AnalysisAnomalyScore(anomaly_score)
 # get top scores
-# normal_number = parser.getint("parameters", "normal_number")
-# anomalous_number = parser.getint("parameters", "anomalous_number")
-
-# [normal_train_idx, anomalous_train_idx] = analyze.top_scores(
-#     number_normal=normal_number,
-#     number_anomalies=anomalous_number
-# )
 normal_number = parser.getint("parameters", "normal_number")
 normal_train_idx = analyze.top_scores(normal_number, anomalous=False)
 
