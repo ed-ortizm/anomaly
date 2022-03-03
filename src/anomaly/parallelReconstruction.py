b8c4be69ab0568b2bec8a7e2d24a10822091f0ff
Edgar Ortiz <ed.ortizm@gmail.com>
2022-03-03 17:05:49 -0300

    Define worker in parallelReconstruction module

--- src/anomaly/parallelReconstruction.py
+++ src/anomaly/parallelReconstruction.py
@@ -19,12 +19,12 @@ def to_numpy_array(array: RawArray, array_shape: tuple) -> np.array:
 ###############################################################################
 def init_shared_data(
     share_counter: mp.Value,
+    share_wave: RawArray,
     share_data: RawArray,
     data_shape: tuple,
     data_location: str,
-    share_architecture: dict,
-    share_hyperparameters: dict,
     share_model_directory: str,
+    share_output_directory: str,
     share_cores_per_worker: int,
 ) -> None:
     """
@@ -36,61 +36,30 @@ def init_shared_data(
         share_data:
         data_shape:
         data_location:
-        share_architecture:
-        share_hyperparameters:
         share_model_directory:
+        share_output_directory:
 
     """
     global counter
+    global wave
     global data
-    global architecture
-    global hyperparameters
+
     global model_directory
+    global output_directory
+
     global cores_per_worker
+    global session
 
     counter = share_counter
-    data = to_numpy_array(share_data, data_shape)
-    data[...] = np.load(data_location)
-    architecture = share_architecture
-    hyperparameters = share_hyperparameters
+    wave =  share_wave
+    observation = to_numpy_array(share_data, data_shape)
+    observation[...] = np.load(data_location)
+
     model_directory = share_model_directory
+    output_directory = share_output_directory
+
     cores_per_worker = share_cores_per_worker
 
-
-###############################################################################
-def compute_anomaly_score(
-) -> None:
-    """
-    PARAMETERS
-        metric:
-        reconstruct_function:
-        wave:
-        lines:
-        velocity_filter:
-        percentage:
-        relative:
-        epsilon:
-    """
-    # Define anomaly score class
-        anomaly = ReconstructionAnomalyScore(
-            reconstruct_function,
-            wave,
-            lines=lines,
-            velocity_filter=velocity_filter,
-            percentage=percentage,
-            relative=relative,
-            epsilon=epsilon,
-        )
-
-        print(f"Score: {score_name}", end="\r")
-
-   #                      score = anomaly.score(observation, metric)
-
-   #                      data_frame[f"{score_name}"] = score
-
-   #                      if save_score is True:
-    #                         np.save(f"{save_to}/{score_name}.npy", score)
-    #                 else:
     ###########################################################################
     import tensorflow as tf
     from autoencoders.ae import AutoEncoder
@@ -104,29 +73,90 @@ def compute_anomaly_score(
         device_count={"CPU": jobs},
     )
     session = tf.compat.v1.Session(config=config)
+    # Define reconstruction function
+
+    model = AutoEncoder(reload=True, reload_from=model_location)
+    reconstruct_function = model.reconstruct
+
+###############################################################################
+def compute_anomaly_score(
+) -> None:
+    """
+    PARAMETERS
+        metric:
+        reconstruct_function:
+        # wave: this is shared parameter
+        # model_directory: shared parameter
+        lines:
+        velocity_filter:
+        percentage:
+        relative:
+        epsilon:
+    """
     ###########################################################################
-    hyperparameters["reconstruction_weight"] = rec_weight
-    hyperparameters["mmd_weight"] = mmd_weight
-    hyperparameters["kld_weight"] = kld_weight
-    hyperparameters["alpha"] = alpha
-    hyperparameters["lambda"] = lambda_
+    # import tensorflow as tf
+    # from autoencoders.ae import AutoEncoder
+    #
+    # # set the number of cores to use per model in each worker
+    # jobs = cores_per_worker
+    # config = tf.compat.v1.ConfigProto(
+    #     intra_op_parallelism_threads=jobs,
+    #     inter_op_parallelism_threads=jobs,
+    #     allow_soft_placement=True,
+    #     device_count={"CPU": jobs},
+    # )
+    # session = tf.compat.v1.Session(config=config)
+    # ###########################################################################
+    # # Define reconstruction function
+    # model = AutoEncoder(reload=True, reload_from=model_location)
+    # reconstruct_function = model.reconstruct
+    ###########################################################################
+    # Define anomaly score class
+    anomaly = ReconstructionAnomalyScore(
+        reconstruct_function,
+        wave,
+        lines=lines,
+        velocity_filter=velocity_filter,
+        percentage=percentage,
+        relative=relative,
+        epsilon=epsilon,
+    )
 
+    # define name of score:
+    # metric_filter_velocity --> has: rel50, noRel75, ...
+    df_name = f"{metric}"
 
+    filter = lines != None
+
+    if filter is True:
+
+        df_name = f"{df_name}_filter_{velocity}Kms"
+
+    # define name of column that will contain the anomaly in the data_frame
+    column_df_name = f"{percentage}"
+
+    if relative is True:
+
+        column_df_name = f"rel{column_df_name}"
+
+    else:
+
+        column_df_name = f"noRel{column_df_name}"
+
+    # define name of array if score is saved
+    score_name = f"{df_name}_{column_df_name}"
+    ###########################################################################
+    # Compute anomaly score
     with counter.get_lock():
 
-        print(f"Start training model {counter.value:04d}", end="\r")
-
-        model_location = f"{model_directory}/{counter.value:04d}"
+        print(f"Compute {score_name}", end="\r")
 
         counter.value += 1
 
-    vae = AutoEncoder(architecture, hyperparameters)
-    vae.train(data)
-    vae.save_model(f"{model_location}")
-
+    score = anomaly.score(observation, metric)
+    np.save_score(f"{output_directory}/{score_name}.npy", score)
+    ###########################################################################
     session.close()
-
-
 ###############################################################################
 def get_parameters_grid(hyperparameters: dict) -> itertools.product:
     """
