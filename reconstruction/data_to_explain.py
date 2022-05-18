3c6a9630348fe6d59584bc9014fdb349093ba720
Edgar Ortiz <ed.ortiz@gmail.com>
2022-05-17 22:02:23 -0400

    chore: save config files when generating top normal and anomalous spectra to explain

--- reconstruction/data_to_explain.py
+++ reconstruction/data_to_explain.py
@@ -1,11 +1,12 @@
+"""Top normal and anomalous spectra"""
 from configparser import ConfigParser, ExtendedInterpolation
 import glob
-import shutil
 import time
 
 import numpy as np
 
-from sdss.superclasses import FileDirectory, ConfigurationFile
+from sdss.utils.managefiles import FileDirectory
+from sdss.utils.configfile import ConfigurationFile
 
 ###############################################################################
 start_time = time.time()
@@ -19,49 +20,59 @@ check = FileDirectory()
 configuration = ConfigurationFile()
 ###############################################################################
 # Load data
-data_directory = parser.get("directory", "data")
-files_location = glob.glob(f"{data_directory}/[a-z]*/*.npy")
+score_directory = parser.get("directory", "score")
 
-print(f"Load anomaly scores", end="\n")
+metric = parser.get("score", "metric")
+filter_velocity = parser.getint("score", "filter")
+relative = parser.getboolean("score", "relative")
+percentage = parser.get("score", "percentage")
 
-percentage = parser.getint("configuration", "percentage")
-percentage /= 100
+score_name = f"{metric}"
+if filter_velocity != 0:
+    score_name = f"{score_name}_filter_{filter_velocity}Kms_"
+if relative is True:
+    score_name = f"{score_name}_rel{percentage}"
+else:
+    score_name = f"{score_name}_noRel{percentage}"
 
-save_to = parser.get("directory", "save_to")
-check.check_directory(save_to, exit=False)
+score_directory = f"{score_directory}/{metric}"
+score = np.load(f"{score_directory}/{score_name}.npy")
 
-for file in files_location:
+print(f"Load anomaly scores: {score_name}", end="\n")
 
-    name_file = file.split("/")[-1].split(".")[0]
+# colums: ..., the actual score
+sort_index = np.argsort(score[:, 2])
+# score = score[sort_index]
 
-    print(f"Load {name_file}", end="\r")
+save_from_index = int(1000)
 
-    score = np.load(file)
+save_score_to = parser.get("directory", "output")
+# check.check_directory(save_to, exit_program=False)
+save_score_to = f"{save_score_to}/{score_name}"
+check.check_directory(save_score_to, exit_program=False)
 
-    # colums: ..., the actual score
-    sort_index = np.argsort(score[:, 2])
-    # score = score[sort_index]
-
-    save_from_index = int(percentage * score.shape[0])
-
-    save_score_to = f"{save_to}/{name_file}"
-    check.check_directory(save_score_to, exit=False)
-
-    np.save(
-        f"{save_score_to}/{name_file}.npy",
-        score[sort_index][-save_from_index:],
-    )
+np.save(
+    f"{save_score_to}/{score_name}.npy",
+    score[sort_index][-save_from_index:],
+)
 
 ###############################################################################
 # Save configuration file
 print(f"Save relevant configuration files", end="\n")
-with open(f"{save_to}/{config_file_name}", "w") as configfile:
+with open(f"{save_score_to}/{config_file_name}", "w") as configfile:
     parser.write(configfile)
 ###############################################################################
-other_config_files = glob.glob(f"{data_directory}/*.ini")
-for file in other_config_files:
-    file_name = file.split("/")[-1]
-    shutil.copyfile(file, f"{save_to}/{file_name}")
+other_config_files = glob.glob(f"{score_directory}/*.ini")
+
+for file_location in other_config_files:
+
+    other_config_file_name = file_location.split("/")[-1]
+
+    with open(file_location, "r") as file:
+        other_config_file = file.read()
+
+    with open(f"{save_score_to}/{other_config_file_name}", "w") as file:
+        file.write(other_config_file)
 ###############################################################################
 finish_time = time.time()
 print(f"\nRun time: {finish_time - start_time:.2f}")
