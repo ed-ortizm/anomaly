3877839d3461aa4dcfe969a1f3d430c5f2184065
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-05-18 18:49:25 -0400

    feature: implement script to get top anomalous and normal spectra
    The script saves an array with the spectra, as well as a data frame with the meta data of the spectra and the anomaly score

--- reconstruction/top_scores.py
+++ reconstruction/top_scores.py
@@ -1,98 +1,116 @@
-"""Compute data frame with top anomalous and normal scores"""
+"""Data frame and array with top normal and anomalous spectra"""
 from configparser import ConfigParser, ExtendedInterpolation
+import glob
 import time
 
 import numpy as np
 import pandas as pd
 
-from anomaly.analysis import AnalysisAnomalyScore
 from sdss.utils.managefiles import FileDirectory
-from sdss.metadata import MetaData
+from sdss.utils.configfile import ConfigurationFile
 
 ###############################################################################
 start_time = time.time()
-###########################################################################
+###############################################################################
 parser = ConfigParser(interpolation=ExtendedInterpolation())
-parser.read("top_scores.ini")
+config_file_name = "data_to_explain.ini"
+parser.read(f"{config_file_name}")
 # Check files and directory
 check = FileDirectory()
+# Handle configuration file
+configuration = ConfigurationFile()
 ###############################################################################
-# Load data
-input_output_directory = parser.get("directories", "input_output")
-anomaly_score_name = parser.get("files", "anomaly_score")
-anomaly_score = np.load(f"{input_output_directory}/{anomaly_score_name}")
+data_directory = parser.get("directory", "data")
+observation_file = parser.get("file", "observation")
+print(f"Load spectra: {observation_file}", end="\n")
+
+observation = np.load(f"{data_directory}/{observation_file}", mmap_mode="r")
 ###############################################################################
-# Load class to analyse anomaies
-analyze = AnalysisAnomalyScore(anomaly_score)
-# get top scores
-normal_number = parser.getint("parameters", "normal_number")
-normal_train_idx = analyze.top_scores(normal_number, anomalous=False)
+score_directory = parser.get("directory", "score")
 
-anomalous_number = parser.getint("parameters", "anomalous_number")
-anomalous_train_idx = analyze.top_scores(anomalous_number, anomalous=True)
+metric = parser.get("score", "metric")
+filter_velocity = parser.getint("score", "filter")
+relative = parser.getboolean("score", "relative")
+percentage = parser.get("score", "percentage")
+
+score_name = f"{metric}"
+if filter_velocity != 0:
+    score_name = f"{score_name}_filter_{filter_velocity}Kms_"
+if relative is True:
+    score_name = f"{score_name}_rel{percentage}"
+else:
+    score_name = f"{score_name}_noRel{percentage}"
+
+print(f"Load anomaly scores: {score_name}", end="\n")
+
+score_directory = f"{score_directory}/{metric}"
+score = np.load(f"{score_directory}/{score_name}.npy")
+
 ###############################################################################
-# Get specobjid from indexes_interpolate array
-train_directory = parser.get("directories", "data")
-train_id_name = parser.get("files", "train_id")
-indexes_interpolate = np.load(f"{train_directory}/{train_id_name}")
-#######################################################################
-meta_data_directory = parser.get("directories", "meta_data")
-spectra_df_name = parser.get("files", "spectra_df")
-spectra_df_location = f"{meta_data_directory}/{spectra_df_name}"
+meta_data_name = parser.get("file", "meta")
 
-spectra_df = pd.read_csv(spectra_df_location, index_col="specobjid")
-#######################################################################
-meta = MetaData()
-# Get data frame for normal objects
-# normal df. Add idTrain, score and url
-specobjid_normal = indexes_interpolate[normal_train_idx][:, 1]
-normal_df = spectra_df.loc[specobjid_normal]
+print(f"Load data frame: {meta_data_name}", end="\n")
 
-normal_df["idTrain"] = normal_train_idx
-# get scores
-scores_normal = anomaly_score[normal_train_idx]
-normal_df["anomalyScore"] = scores_normal
-# Get urls in sdss dr16 object explorer
-sdss_explorer_urls = []
+meta_data_directory = parser.get("directory", "meta")
 
-for specobjid in specobjid_normal:
-
-    url = meta.get_sky_server_url(specobjid)
-    sdss_explorer_urls.append(url)
-
-normal_df["explorerUrl"] = sdss_explorer_urls
-
-#######################################################################
-df_name = parser.get("files", "normal_df")
-save_to = f"{input_output_directory}/{df_name}"
-normal_df.to_csv(save_to, index=True)
+meta_data = pd.read_csv(
+    f"{meta_data_directory}/{meta_data_name}", index_col="specobjid"
+)
+bin_specobjid = score[:, 0].astype(int)
+meta_data = meta_data.loc[bin_specobjid]
 ###############################################################################
-# Get data frame for normal objects
-# normal df. Add idTrain, score and url
-specobjid_anomalous = indexes_interpolate[anomalous_train_idx][:, 1]
-anomalous_df = spectra_df.loc[specobjid_anomalous]
+save_score_to = parser.get("directory", "output")
+save_score_to = f"{save_score_to}/{score_name}"
+check.check_directory(save_score_to, exit_program=False)
+# colums: ..., the actual score
+sort_index = np.argsort(score[:, 2])
+bin_specobjid = bin_specobjid[sort_index]
 
-anomalous_df["idTrain"] = anomalous_train_idx
-# get scores
-scores_anomalous = anomaly_score[anomalous_train_idx]
-anomalous_df["anomalyScore"] = scores_anomalous
-# Get urls in sdss dr16 object explorer
-sdss_explorer_urls = []
+print("Fetch normal spectra",end="\n")
+number_normal = parser.getint("spectra", "top_normal")
+normal_spectra_index = sort_index[:number_normal]
 
-for specobjid in specobjid_anomalous:
+np.save(
+    f"{save_score_to}/top_normal.npy",
+    observation[normal_spectra_index],
+)
+specobjid_normal = bin_specobjid[:number_normal]
 
-    url = meta.get_sky_server_url(specobjid)
-    sdss_explorer_urls.append(url)
-
-anomalous_df["explorerUrl"] = sdss_explorer_urls
-
-#######################################################################
-df_name = parser.get("files", "anomalous_df")
-save_to = f"{input_output_directory}/{df_name}"
-anomalous_df.to_csv(save_to, index=True)
-print(save_to)
+normal_meta_data = meta_data.loc[specobjid_normal]
+normal_meta_data["score"] = score[sort_index, 2][:number_normal]
+normal_meta_data.to_csv(f"{save_score_to}/top_normal.csv.gz")
 ###############################################################################
-# print(analyze.get_percentiles())
+print("Fetch anomalies",end="\n")
+number_anomalies = parser.getint("spectra", "top_anomalies")
+anomalies_spectra_index = sort_index[-1 * number_anomalies:]
+np.save(
+    f"{save_score_to}/top_anomalies.npy",
+    observation[anomalies_spectra_index],
+)
+specobjid_anomalies = bin_specobjid[-1 * number_anomalies:]
+
+anomalies_meta_data = meta_data.loc[specobjid_anomalies]
+anomalies_meta_data["score"] = score[sort_index, 2][-1 * number_anomalies:]
+anomalies_meta_data.to_csv(
+    f"{save_score_to}/top_anomalies.csv.gz"
+)
+###############################################################################
+# Save configuration file
+print(f"Save relevant configuration files", end="\n")
+with open(f"{save_score_to}/{config_file_name}", "w") as configfile:
+    parser.write(configfile)
+###############################################################################
+other_config_files = glob.glob(f"{score_directory}/*.ini")
+
+for file_location in other_config_files:
+
+    other_config_file_name = file_location.split("/")[-1]
+
+    with open(file_location, "r") as file:
+        other_config_file = file.read()
+
+    with open(f"{save_score_to}/{other_config_file_name}", "w") as file:
+        file.write(other_config_file)
 ###############################################################################
 finish_time = time.time()
-print(f"Run time: {finish_time - start_time:.2f}")
+print(f"\nRun time: {finish_time - start_time:.2f}")
