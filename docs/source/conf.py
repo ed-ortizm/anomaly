5cb04b8bbec13b0bca9dd582fddd888fc8b0b766
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-12-31 16:42:20 -0500

    feature: add documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../src/anomaly/'))
+
+extensions = ['sphinx.ext.autodoc']
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'anomaly detection with VAEs and SDSS spectra'
+copyright = '2022, Edgar Ortiz'
+author = 'Edgar Ortiz'
+
+html_theme = 'alabaster'
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['_static']
